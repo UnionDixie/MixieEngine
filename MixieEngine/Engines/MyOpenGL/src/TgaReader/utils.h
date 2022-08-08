bafd27a102b5ea1355d9afc6332d64750e58d053
UnionDixie <77199764+UnionDixie@users.noreply.github.com>
2022-08-08 23:52:31 +0300

    Draw obj

--- MixieEngine/Engines/MyOpenGL/src/TgaReader/utils.h
+++ MixieEngine/Engines/MyOpenGL/src/TgaReader/utils.h
@@ -13,24 +13,32 @@ namespace Tga {
 	void line(Point p1, Point p2, TgaImage& image, const TGAColor color)
 	{
         bool steep = false;
-        if (std::abs(p1.x - p2.x) < std::abs(p1.y - p2.y)) { // if the line is steep, we transpose the image
+        if (std::abs(p1.x - p2.x) < std::abs(p1.y - p2.y)) {
             std::swap(p1.x, p1.y);
             std::swap(p2.x, p2.y);
             steep = true;
         }
-        if (p1.x > p2.x) { // make it left-to-right
+        if (p1.x > p2.x) {
             std::swap(p1.x, p2.x);
             std::swap(p1.y, p2.y);
         }
-
+        int dx = p2.x - p1.x;
+        int dy = p2.y - p1.y;
+        int derror2 = std::abs(dy) * 2;
+        int error2 = 0;
+        int y = p1.y;
         for (int x = p1.x; x <= p2.x; x++) {
-            float t = (x - p1.x) / (float)(p2.x - p1.x);
-            int y = p1.y * (1. - t) + p2.y * t;
             if (steep) {
-                image.set(y, x, color); // if transposed, de-transpose
+                image.set(y, x, color);
             }
             else {
                 image.set(x, y, color);
+            }
+            error2 += derror2;
+
+            if (error2 > dx) {
+                y += (p2.y > p1.y ? 1 : -1);
+                error2 -= dx * 2;
             }
         }
 	}
--- MixieEngine/Engines/MyOpenGL/src/main.cpp
+++ MixieEngine/Engines/MyOpenGL/src/main.cpp
@@ -1,23 +1,42 @@
+#include <vector>
+#include <cmath>
 #include "TgaReader/tgaimage.h"
 #include "TgaReader/utils.h"
+#include "Model/model.h"
+#include "Model/geometry.h"
 
 using namespace Tga;
+
 const TGAColor white = TGAColor(255, 255, 255, 255);
 const TGAColor red = TGAColor(255, 0, 0, 255);
+Model* model = NULL;
+const int width = 800;
+const int height = 800;
 
-int main(int argc, char** argv)
-{
-	TgaImage image(100, 100, TgaImage::RGB);
-	image.set(52, 41, red);
-	image.flip_vertically(); // i want to have the origin at the left bottom corner of the image
-	//image.flip_vertically();
-	//line2(Point{ 10, 10 }, Point{ 70, 70 }, image, white);
+int main(int argc, char** argv) {
+    if (2 == argc) {
+        model = new Model(argv[1]);
+    }
+    else {
+        model = new Model("src/obj/african_head.obj");
+    }
 
-	line(Point{13, 20}, Point{80, 40}, image, white);
-	line(Point{20, 13}, Point{40, 80}, image, red);
-	line(Point{80, 40}, Point{13, 20}, image, red);
+    TgaImage image(width, height, TgaImage::RGB);
+    for (int i = 0; i < model->nfaces(); i++) {
+        std::vector<int> face = model->face(i);
+        for (int j = 0; j < 3; j++) {
+            Vec3f v0 = model->vert(face[j]);
+            Vec3f v1 = model->vert(face[(j + 1) % 3]);
+            int x0 = (v0.x + 1.) * width / 2.;
+            int y0 = (v0.y + 1.) * height / 2.;
+            int x1 = (v1.x + 1.) * width / 2.;
+            int y1 = (v1.y + 1.) * height / 2.;
+            line(Point{ x0, y0 }, Point{ x1, y1 }, image, white);
+        }
+    }
 
-	image.write_tga_file("output.tga");
-
-	return 0;
+    image.flip_vertically(); // i want to have the origin at the left bottom corner of the image
+    image.write_tga_file("output.tga");
+    delete model;
+    return 0;
 }
